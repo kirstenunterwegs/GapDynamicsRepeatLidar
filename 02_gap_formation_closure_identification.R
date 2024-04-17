53edb34a51c468e86d30ef00c617b7cf9f32275b
Kirsten Krueger <kirsten.krueger@posteo.de>
2024-04-17 09:52:21 +0200

    update comments

--- 02_gap_formation_closure_identification.R
+++ 02_gap_formation_closure_identification.R
@@ -11,7 +11,6 @@ library(tidyr)
 library(terra)
 
 
-
 # --- load gap layers ----
 
 gap_stack <- rast("data/processed/gaps_final/gaps_masked.tif")
@@ -46,6 +45,7 @@ gap_change_class <- function(gap_layer1, gap_layer2){
 } 
 
 
+
 exp_clo_917 <- gap_change_class(gaps2009, gaps2017) # between 2009 and 2017
 exp_clo_1721 <- gap_change_class(gaps2017, gaps2021) # between 2017 and 2021
 
@@ -57,7 +57,7 @@ writeRaster(exp_clo_1721, "data/processed/gap_change/formation_closure_1721.tif"
 # --- extract vegetation growth in gap closure areas per time step ---
 
 # CHM cannot be provided, but resulting growth values within gap area
-# is provided in zenodo
+# is provided in accompanying zenodo data set
 
 chm9 <- rast("data/processed/CHM_data/chm9_artifacts_masked.tif")
 chm17 <- rast("data/processed/CHM_data/chm17_artifacts_masked.tif")
@@ -85,4 +85,3 @@ clo_growth_1721 <-mask(diff1721, clo_1721)
 
 writeRaster(clo_growth_917 , "data/processed/closure/closure_area_growth_917.tif") # data provided!
 writeRaster(clo_growth_1721 , "data/processed/closure/closure_area_growth_1721.tif")
-
